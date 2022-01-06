102fa45143520bea19bd9c300319798eca6f674b
hmartiins <hmartins224@gmail.com>
2022-01-05 22:11:16 -0300

    Create endpoint to create Todos for Post Method

--- Program.cs
+++ Program.cs
@@ -1,13 +1,28 @@
 using MiniTodo.Data;
+using MiniTodo.ViewModels;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
-app.MapGet("v1/todos", (AppDbContext context) => 
+app.MapGet("v1/todos", (AppDbContext context) =>
 {
     var todos = context.Todos.ToList();
     return Results.Ok(todos);
+});
+
+app.MapPost("v1/todos", (AppDbContext context, CreateTodoViewModel model) =>
+{
+    var todo = model.MapTo();
+    if (!model.IsValid)
+    {
+        return Results.BadRequest(model.Notifications);
+    }
+
+    context.Todos.Add(todo);
+    context.SaveChanges();
+
+    return Results.Created($"/v1/todos/{todo.Id}", todo);
 });
 
 app.Run();
